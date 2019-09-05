8dd85a6c275f4e64e42a30a6b908d3f855fd29a8
Maurin Ludwig <maurinludwig@gmail.com>
2019-09-05 15:39:08 +0200

    Adding gdx config

---
+++ desktop/src/com/nimasi/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.nimasi.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.nimasi.game.NimasiJumper;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new NimasiJumper(), config);
+	}
+}
