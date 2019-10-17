ed0c19ccd364653521922789140acae01b17f2e5
Maurin Ludwig <maurin.ludwig@hostpoint.ch>
2019-10-17 15:34:10 +0200

    EDU-120 removed debug function

--- core/src/com/nimasi/game/screens/GameScreen.java
+++ core/src/com/nimasi/game/screens/GameScreen.java
@@ -4,10 +4,8 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
-import com.badlogic.gdx.math.Vector3;
 import com.nimasi.game.NimasiJumper;
 import com.nimasi.game.world.GameMap;
-import com.nimasi.game.world.TileType;
 import com.nimasi.game.world.TiledGameMap;
 
 /**
@@ -44,18 +42,7 @@ public class GameScreen implements Screen {
     public void render(float delta) {
         Gdx.gl.glClearColor(0.2f, 0.2f, 0.2f, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-        if (Gdx.input.justTouched()) {
-//			Gets position in game world
-            Vector3 pos = cam.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
-
-            TileType type = gameMap.getTileTypeByLocation(1, pos.x, pos.y);
-
-            if (type != null) {
-                System.out.println("You clicked on tile with id " + type.getId() + " " + type.getName() + " " + type.isCollidable() + " " + type.getName());
-            }
-        }
-
+        
         gameMap.update(Gdx.graphics.getDeltaTime());
         gameMap.render(cam, game.batch);
     }
