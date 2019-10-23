fe00ffe68461a2da15634d9fbea60c34286e7db2
Maurin Ludwig <maurin.ludwig@hostpoint.ch>
2019-10-23 15:32:10 +0200

    remove old gradle project

--- core/assets/untitled.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="nimasi" tilewidth="16" tileheight="16" tilecount="6" columns="6">
- <image source="tiles.png" width="96" height="16"/>
-</tileset>
