087484e737a5d354b798d504934c97d3977d27ca
Maurin Ludwig <maurin.ludwig@hostpoint.ch>
2019-11-04 14:15:16 +0100

    EDU-118 added background music

--- core/src/main/java/com/nimasi/game/core/NimasiJumper.java
+++ core/src/main/java/com/nimasi/game/core/NimasiJumper.java
@@ -1,6 +1,8 @@
 package com.nimasi.game.core;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.nimasi.game.core.screens.MenuScreen;
 
@@ -13,6 +15,7 @@ public class NimasiJumper extends Game {
     public static final int HEIGHT = 720;
 
     public SpriteBatch batch;
+    public Music backgroundMusic;
 
 
     /**
@@ -20,6 +23,10 @@ public class NimasiJumper extends Game {
      */
     @Override
     public void create() {
+        backgroundMusic = Gdx.audio.newMusic(Gdx.files.internal("music.mp3"));
+        backgroundMusic.setLooping(true);
+        backgroundMusic.setVolume(0.01f);
+        backgroundMusic.play();
         batch = new SpriteBatch();
         this.setScreen(new MenuScreen(this));
 
