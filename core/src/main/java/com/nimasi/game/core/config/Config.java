63bb0273c4ed28575b3367750756f2737fef421d
Maurin Ludwig <maurin.ludwig@hostpoint.ch>
2019-11-08 09:57:57 +0100

    EDU-119 added javadoc

--- core/src/main/java/com/nimasi/game/core/config/Config.java
+++ core/src/main/java/com/nimasi/game/core/config/Config.java
@@ -1,40 +1,77 @@
 package com.nimasi.game.core.config;
 
+/**
+ * Defines Config Object
+ */
 public class Config {
 
     private String username;
     private float musicVol;
     private float soundVol;
 
-    public Config() {
-    }
-
+    /**
+     * Config Constructor
+     *
+     * @param name String: Username
+     * @param soundVol float: Volume of sounds
+     * @param musicVol float: Volume of music
+     */
     public Config(String name, float soundVol, float musicVol) {
         this.username = name;
         this.soundVol = soundVol;
         this.musicVol = musicVol;
     }
 
+    /**
+     * Gets username
+     *
+     * @return String: Username
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Sets username
+     *
+     * @param username String: Username
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Gets music vol
+     *
+     * @return float: Current music volume
+     */
     public float getMusicVol() {
         return musicVol;
     }
 
+    /**
+     * Sets music vol
+     *
+     * @param musicVol float: New music volume
+     */
     public void setMusicVol(float musicVol) {
         this.musicVol = musicVol;
     }
 
+    /**
+     * Gets sound vol
+     *
+     * @return float: Current sound volume
+     */
     public float getSoundVol() {
         return soundVol;
     }
 
+    /**
+     * Sets sound vol
+     *
+     * @param soundVol float: New sound volume
+     */
     public void setSoundVol(float soundVol) {
         this.soundVol = soundVol;
     }
--- core/src/main/java/com/nimasi/game/core/config/ConfigManager.java
+++ core/src/main/java/com/nimasi/game/core/config/ConfigManager.java
@@ -4,11 +4,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.utils.Json;
 import com.badlogic.gdx.utils.JsonWriter;
-import com.nimasi.game.core.highscore.Highscore;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class ConfigManager {
 
